85ef6c13dde3bd31dab4a0a682b397a350c50025
Jesus E. Angeles <jsus.159@gmail.com>
2012-07-05 14:53:42 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Slik::Application.config.secret_token = '359ebe32b6c8d23e0a5df171799d095a5f88f3c5888d380fbec2c9f05c4fca6260df86aae5b173889cb052091ff9ee99811132a7ae299cd33632c9487ae2d219'
