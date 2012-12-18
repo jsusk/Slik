a55313ac4299de0636cf5ee285a01988cdb6c94e
Jesus E. Angeles <jsus.159@gmail.com>
2012-12-17 22:40:26 -0600

    Ajustes a request

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,4 +2,8 @@ class Request < ActiveRecord::Base
   belongs_to :product
   belongs_to :user
   attr_accessible :amount, :status, :recibido, :enviado, :product_id
+
+  def product_name
+    self.product.name
+  end
 end
